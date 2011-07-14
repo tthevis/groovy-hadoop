8a075ff0056ea2ec377229ee3418846492c7b53a
Thomas Thevis <thomas@thevis.net>
2011-07-14 23:36:38 +0200

    implemented proper wordcount test

--- src/test/groovy/net/thevis/groovyhadoop/MainTest.groovy
+++ src/test/groovy/net/thevis/groovyhadoop/MainTest.groovy
@@ -18,18 +18,39 @@ package net.thevis.groovyhadoop;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
+
 /**
  * @author Thomas Thevis
  *
  */
 class MainTest extends GroovyTestCase {
 
-	void testMain() {
+	private File testOutput
+	
+	void setUp() {
+		this.testOutput = new File('build/test-main')
+	}
+	
+	void tearDown() {
+		if (this.testOutput?.exists()) {
+			this.testOutput.deleteDir()
+		}
+	}
+	
+	void testWordcount() {
 		Main.main((String[])[
 			'-D', 'mapred.output.key.class=org.apache.hadoop.io.Text', 
 			'-D', 'mapred.output.value.class=org.apache.hadoop.io.LongWritable', 
 			'-map', 'value.toString().tokenize().each { context.write(new Text(it), new LongWritable(1)) }',
 			'-reduce', 'def sum = 0; values.each { sum += it.get() }; context.write(key, new LongWritable(sum))', 
-			'-input', './LICENSE-2.0', '-output', './test-output'])
+			'-input', './LICENSE-2.0', '-output', this.testOutput.path])
+		
+		def result = new File(this.testOutput, "part-r-00000").text
+		
+		def expectedResult = getClass().getClassLoader()
+			.getResourceAsStream('wordcount.result').text
+		assert result == expectedResult
 	}
 }
