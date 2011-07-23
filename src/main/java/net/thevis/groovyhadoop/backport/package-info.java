171a82d07a993a923e629b8f33c08b9216db3951
Thomas Thevis <thomas@thevis.net>
2011-07-23 22:30:27 +0200

    added backports from hadoop-0.21 to allow usage of CombineFileInputFormat with new map/reduce API even with hadoop-0.20.x

---
+++ src/main/java/net/thevis/groovyhadoop/backport/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes taken and backported from apache-hadoop-0.21.0 with
+ * minor modifications on packaging and imports to be able to use 
+ * {@link net.thevis.groovyhadoop.backport.CombineFileInputFormat} even for
+ * hadoop-0.20.x clusters. 
+ */
+package net.thevis.groovyhadoop.backport;
